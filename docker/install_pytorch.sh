7f3df78a8548e196e3d7ecf99c11adaf5dfc7814
Yixuan Wang <yixuanwa@umich.edu>
2021-02-19 02:39:52 -0500

    understnad mismatching and re-test

--- docker/install_pytorch.sh
+++ docker/install_pytorch.sh
@@ -10,4 +10,6 @@ pip install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp27-cp27mu
 # pip install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp27-cp27m-linux_x86_64.whl
 # pip install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp27-cp27m-linux_x86_64.whl
 
-
+# pytorch 1.7 CUDA 11.0
+# apt-get -y install python3-pip
+# pip3 install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio===0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
